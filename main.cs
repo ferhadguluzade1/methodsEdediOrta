c87325e6ba3f0be2e85778533013935c2207f7f8
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-16 10:23:03 +0400

    main.cs

---
+++ main.cs
@@ -0,0 +1,20 @@
+static void edediOrta(int[] a)
+{
+    Console.WriteLine("Array-i daxil et");
+    a = Console.ReadLine();
+    int cem;
+    for(int=0; int<a.Length; int++)
+    {
+        cem += a[i];
+
+    }
+    float edediorta = cem / (a.Length);
+    Console.WriteLine(edediorta);
+    
+
+}
+static void Main(string[] args);
+{
+    edediOrta ={ 1,2,3,4,5,6,7,8,9,10};
+    Console.WriteLine(edediOrta);
+}
